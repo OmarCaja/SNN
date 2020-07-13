5993d6695553f604b03b82b295bf8acee6c59282
Omar <omar.caja@hotmail.com>
2020-07-13 23:15:58 +0200

    Added MNN attributed

--- src/simple_neural_network/neural_systems/multilayer_neural_network/multilayer_neural_network.py
+++ src/simple_neural_network/neural_systems/multilayer_neural_network/multilayer_neural_network.py
@@ -1,13 +1,41 @@
+from simple_neural_network.constants import constants
 from simple_neural_network.neuron.neuron import Neuron
 
 
 class MultilayerNeuralNetwork:
-    dimension = 0
-    activation_function = 1
 
     def __init__(self, layers_definition):
-        self.__number_of_classes = layers_definition[-1][MultilayerNeuralNetwork.dimension]
-        self.__layers = [[Neuron(layers_definition[layer - 1][MultilayerNeuralNetwork.dimension],
-                                 layers_definition[layer][MultilayerNeuralNetwork.activation_function])
-                          for _ in range(layers_definition[layer][MultilayerNeuralNetwork.dimension])]
-                         for layer in range(1, len(layers_definition))]
+        self.__number_of_classes = layers_definition[-1][constants.MULTILAYER_NEURAL_NETWORK.get('NEURON_DIMENSION')]
+        self.__layers = [
+            [
+                Neuron(
+                    layers_definition[layer - 1][constants.MULTILAYER_NEURAL_NETWORK.get('NEURON_DIMENSION')],
+                    layers_definition[layer][constants.MULTILAYER_NEURAL_NETWORK.get('NEURON_ACTIVATION_FUNCTION')]
+                )
+                for _ in range(layers_definition[layer][constants.MULTILAYER_NEURAL_NETWORK.get('NEURON_DIMENSION')])
+            ]
+            for layer in range(1, len(layers_definition))
+        ]
+        self.__learning_rate = constants.MULTILAYER_NEURAL_NETWORK.get('LEARNING_RATE_DEFAULT_VALUE')
+        self.__max_epochs = constants.MULTILAYER_NEURAL_NETWORK.get('MAX_EPOCHS_DEFAULT_VALUE')
+        self.__miss_classified_samples_per_epoch = []
+
+    @property
+    def number_of_classes(self):
+        return self.__number_of_classes
+
+    @property
+    def learning_rate(self):
+        return self.__learning_rate
+
+    @property
+    def max_epochs(self):
+        return self.__max_epochs
+
+    @property
+    def miss_classified_samples_per_epoch(self):
+        return self.__miss_classified_samples_per_epoch
+
+    @property
+    def weights(self):
+        return [[neuron.weights for neuron in layer] for layer in self.__layers]
